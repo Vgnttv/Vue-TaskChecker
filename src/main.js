73bc2dd3e1057000b5184ebec36deffb7ba9eb35
Vgnttv <o.vignani@gmail.com>
2019-08-08 12:59:48 +0200

    Rewrote the routing

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import router from './router'
+import router from './router.js'
 
 
 
@@ -11,5 +11,6 @@ Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 
 new Vue({
-  render: h => h(App),router
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/router.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/overview",
+      name: "Overview",
+      component: () => import("./components/Overview.vue")
+    },
+    // {
+    //   path: "/tasks",
+    //   name: "Tasks",
+    //   component: () => import("./components/Tasks.vue")
+    // }
+  ]
+});
+ 
